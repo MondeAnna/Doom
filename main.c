4eb88531f7f9f4ad36b88a980c9e9cf9eed465c0
MondeAnna <monde.anna@gmail.com>
2024-06-16 17:45:42 +0200

    player (init): provide moveable square shaped player

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <SDL2/SDL.h>
 
 #define FIVE_SECONDS	5000
+#define PLAYER_SIZE	10
 #define SCREEN_WIDTH    1360
 #define SCREEN_HEIGHT   768
+#define TRUE		1
+#define FALSE		0
+
+/**
+ * handleEvent - processes event/input
+ * @event: the event/input
+ * @player: the player object
+ * Return: FALSE if user quits, else TRUE
+ */
+int handleEvent(SDL_Event *event, SDL_Rect *player)
+{
+	if (SDL_PollEvent(event))
+	{
+		if (event->type == SDL_QUIT)
+			return (FALSE);
+
+		/* swap the arrows for a, s, d, w */
+		/* SDL_GetKeyName(event.key.keysym.sym) */
+		if (event->type == SDL_KEYDOWN)
+			if (event->key.keysym.sym == SDLK_UP)
+				player->y-=PLAYER_SIZE;
+			else if (event->key.keysym.sym == SDLK_DOWN)
+				player->y+=PLAYER_SIZE;
+			else if (event->key.keysym.sym == SDLK_LEFT)
+				player->x-=PLAYER_SIZE;
+			else if (event->key.keysym.sym == SDLK_RIGHT)
+				player->x+=PLAYER_SIZE;
+	}
+
+	return (TRUE);
+}
 
 /**
  * main - entry point
@@ -15,6 +47,7 @@
  */
 int main(int argc, char **argv)
 {
+	SDL_Event event;
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 
@@ -26,12 +59,19 @@ int main(int argc, char **argv)
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
-	/* set colour to white */
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderDrawPoint(renderer, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+	SDL_Rect player = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_SIZE, PLAYER_SIZE};
 
-	SDL_RenderPresent(renderer);
-	SDL_Delay(5000);
+	do {
+		/* set colour to white */
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderDrawRect(renderer, &player);
+		SDL_RenderPresent(renderer);
+
+		/* set colour to black */
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+	} while(handleEvent(&event, &player));
 
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);

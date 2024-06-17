8f9a5504512f6b20699079fb586bec3205af9373
MondeAnna <monde.anna@gmail.com>
2024-06-17 13:40:48 +0200

    proj (refactor): changes according to betty and compiler
        - compilation has been changed to to:
    	- gcc main.c -Wall -Werror -Wextra -pedantic -lSDL2 -o main && ./main

--- main.c
+++ main.c
@@ -1,11 +1,64 @@
 #include <SDL2/SDL.h>
 
 #define FIVE_SECONDS	5000
-#define PLAYER_SIZE	10
-#define SCREEN_WIDTH    1360
-#define SCREEN_HEIGHT   768
+#define SIZE	10
+#define WIDTH    1360
+#define HEIGHT   768
 #define TRUE		1
 #define FALSE		0
+
+/**
+ * darkenBackdrop - darkens the backdrop
+ * @renderer: backdrop to be darkened
+ * Return: void
+ */
+void darkenBackdrop(SDL_Renderer *renderer)
+{
+	/* set colour to black */
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+}
+
+/**
+ * drawMap - draws map onto the renderer
+ * @renderer: the backdrop in use
+ * Return: void
+ */
+void drawMap(SDL_Renderer *renderer)
+{
+	int col, row;
+	int map[][8] = {
+		{1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 1, 0, 1},
+		{1, 0, 1, 0, 1, 0, 1},
+		{1, 0, 1, 0, 1, 0, 1},
+		{1, 0, 1, 1, 1, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1}
+	};
+
+	SDL_Rect block = {0, 0, SIZE * 2, SIZE * 2};
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			if (map[row][col])
+				/* set colour to white */
+				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			else
+				/* set colour to black */
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+			block.x = row;
+			block.y = col;
+
+			SDL_RenderDrawRect(renderer, &block);
+			SDL_RenderPresent(renderer);
+		}
+	}
+}
 
 /**
  * drawPlayer - draws player onto the renderer
@@ -37,30 +90,19 @@ int handleEvent(SDL_Event *event, SDL_Rect *player)
 		/* swap the arrows for a, s, d, w */
 		/* SDL_GetKeyName(event.key.keysym.sym) */
 		if (event->type == SDL_KEYDOWN)
+		{
 			if (event->key.keysym.sym == SDLK_UP)
-				player->y-=PLAYER_SIZE;
+				player->y -= SIZE;
 			else if (event->key.keysym.sym == SDLK_DOWN)
-				player->y+=PLAYER_SIZE;
+				player->y += SIZE;
 			else if (event->key.keysym.sym == SDLK_LEFT)
-				player->x-=PLAYER_SIZE;
+				player->x -= SIZE;
 			else if (event->key.keysym.sym == SDLK_RIGHT)
-				player->x+=PLAYER_SIZE;
+				player->x += SIZE;
+		}
 	}
 
 	return (TRUE);
-}
-
-
-/**
- * darkenBackdrop - darkens the backdrop
- * @renderer: backdrop to be darkened
- * Return: void
- */
-void darkenBackdrop(SDL_Renderer *renderer)
-{
-	/* set colour to black */
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
 }
 
 /**
@@ -72,22 +114,24 @@ void darkenBackdrop(SDL_Renderer *renderer)
  *
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(void)
 {
 	SDL_Event event;
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 
 	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
+	SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
 	SDL_SetWindowTitle(window, "Doom by Monde Anna");
 
-	SDL_Rect player = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_SIZE, PLAYER_SIZE};
+	SDL_Rect player = {WIDTH / 2, HEIGHT / 2, SIZE, SIZE};
+
+	drawMap(renderer);
 
 	do {
 		darkenBackdrop(renderer);
 		drawPlayer(renderer, &player);
-	} while(handleEvent(&event, &player));
+	} while (handleEvent(&event, &player));
 
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
